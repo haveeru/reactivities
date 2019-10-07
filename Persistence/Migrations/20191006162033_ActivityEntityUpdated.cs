8bc6accbaff4c4e469ffb9a89319b20fa34641a0
haveeru <suood_mohamed@hotmail.com>
2019-10-07 12:29:24 +0500

    section 4 completed

---
+++ Persistence/Migrations/20191006162033_ActivityEntityUpdated.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class ActivityEntityUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
